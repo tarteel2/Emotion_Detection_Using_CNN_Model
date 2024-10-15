a6246010445e9c5353ca47303dc0e4dbb1008933
tarteel2 <tuti.alkaraan.22@gmail.com>
2024-10-15 10:32:06 +0100

    Solution

--- Model/yolov2.py
+++ Model/yolov2.py
@@ -87,85 +87,86 @@ yolo_model.compile(optimizer = Adam(learning_rate = 0.0001), loss = "categorical
 #yolo_model.summary()
 
 #Configure Checkpoint Model
-fle_s = 'Model/Output/Emotion_Model.keras'
-checkpointer = ModelCheckpoint(fle_s, monitor = 'loss', verbose = 1, save_best_only = True, save_weights_only = False, mode = 'auto', save_freq = 'epoch')
-callback_list = [checkpointer]
+# fle_s = 'Model/Output/Emotion_Model.keras'
+# checkpointer = ModelCheckpoint(fle_s, monitor = 'loss', verbose = 1, save_best_only = True, save_weights_only = False, mode = 'auto', save_freq = 'epoch')
+# callback_list = [checkpointer]
 
 save = yolo_model.fit(X_train, Y_train, batch_size = 32, validation_data = (X_test, Y_test), epochs = 100, callbacks = [callback_list])
 
-#Checking train and test loss and accuracy values from above neural network
-train_loss = save.history['loss']
-test_loss = save.history['val_loss']
-train_accuracy = save.history['accuracy']
-test_accuracy = save.history['val_accuracy']
+# #Checking train and test loss and accuracy values from above neural network
+# train_loss = save.history['loss']
+# test_loss = save.history['val_loss']
+# train_accuracy = save.history['accuracy']
+# test_accuracy = save.history['val_accuracy']
 
-#Plotting line chart to visualize loss and accuracy values by epochs
-fig, ax = plt.subplots(ncols = 2, figsize = (15, 7))
-ax = ax.ravel()
-ax[0].plot(train_loss, label = 'Train Loss', color = 'royalblue', marker = 'o', markersize = 5)
-ax[0].plot(test_loss, label = 'Test Loss', color = 'orangered', marker = 'o', markersize = 5)
-ax[0].set_xlabel('Epochs', fontsize = 14)
-ax[0].set_ylabel('Categorical Crossentropy', fontsize = 14)
-ax[0].legend(fontsize = 14)
-ax[0].tick_params(axis = 'both', labelsize = 12)
+# #Plotting line chart to visualize loss and accuracy values by epochs
+# fig, ax = plt.subplots(ncols = 2, figsize = (15, 7))
+# ax = ax.ravel()
+# ax[0].plot(train_loss, label = 'Train Loss', color = 'royalblue', marker = 'o', markersize = 5)
+# ax[0].plot(test_loss, label = 'Test Loss', color = 'orangered', marker = 'o', markersize = 5)
+# ax[0].set_xlabel('Epochs', fontsize = 14)
+# ax[0].set_ylabel('Categorical Crossentropy', fontsize = 14)
+# ax[0].legend(fontsize = 14)
+# ax[0].tick_params(axis = 'both', labelsize = 12)
 
-ax[1].plot(train_accuracy, label = 'Train Accuracy', color = 'royalblue', marker = 'o', markersize = 5)
-ax[1].plot(test_accuracy, label = 'Test Accuracy', color = 'orangered', marker = 'o', markersize = 5)
-ax[1].set_xlabel('Epochs', fontsize = 14)
-ax[1].set_ylabel('Accuracy', fontsize = 14)
-ax[1].legend(fontsize = 14)
-ax[1].tick_params(axis = 'both', labelsize = 12)
-fig.suptitle(x = 0.5, y = 0.92, t = "Lineplots Showing Loss and Accuracy of CNN Model by Epochs", fontsize = 16)
+# ax[1].plot(train_accuracy, label = 'Train Accuracy', color = 'royalblue', marker = 'o', markersize = 5)
+# ax[1].plot(test_accuracy, label = 'Test Accuracy', color = 'orangered', marker = 'o', markersize = 5)
+# ax[1].set_xlabel('Epochs', fontsize = 14)
+# ax[1].set_ylabel('Accuracy', fontsize = 14)
+# ax[1].legend(fontsize = 14)
+# ax[1].tick_params(axis = 'both', labelsize = 12)
+# fig.suptitle(x = 0.5, y = 0.92, t = "Lineplots Showing Loss and Accuracy of Yolo Model by Epochs", fontsize = 16)
+# plt.show()
+
+#Plotting confusion matix and roc curve of model
+dir = "Model/Output/Emotion_Model.keras"
+model = load_model(dir)
+best_model = model
+
+Y_pred = best_model.predict(X_test)
+Y_test_labels = np.argmax(Y_test, axis = 1)
+Y_pred_labels = np.argmax(Y_pred, axis = 1)
+
+cm = confusion_matrix(np.argmax(Y_test, axis = 1), np.argmax(Y_pred, axis = 1))
+disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = ["Happy", "Sad"])
+disp.plot()
+
+plt.title('My Yolo Emotion Classifer')
+plt.ylabel('Actual class')
+plt.xlabel('Predicted class')
+
+plt.gca().xaxis.set_label_position('top')
+plt.gca().xaxis.tick_top()
+
 plt.show()
 
-# #Plotting confusion matix and roc curve of model
-# dir = "Model/Output/Emotion_Model.keras"
-# model = load_model(dir)
-# best_model = model
+#ROC Curve
+new_label = ['Happy', 'Sad']
+final_label = new_label
+new_class = 2
 
-# Y_pred = best_model.predict(X_test)
-# Y_test_labels = np.argmax(Y_test, axis = 1)
-# Y_pred_labels = np.argmax(Y_pred, axis = 1)
+#Ravel flatten array into one vector
+y_pred_ravel = Y_pred.ravel()
+lw = 2
 
-# cm = confusion_matrix(np.argmax(Y_test, axis = 1), np.argmax(Y_pred, axis = 1))
-# disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = ["Happy", "Sad"])
-# disp.plot()
-# plt.title('My CNN Emotion Classifer')
-# plt.ylabel('Actual class')
-# plt.xlabel('Predicted class')
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
 
-# plt.gca().xaxis.set_label_position('top')
-# plt.gca().xaxis.tick_top()
-
-# plt.show()
-
-# #ROC Curve
-# new_label = ['Happy', 'Sad']
-# final_label = new_label
-# new_class = 2
-
-# #Ravel flatten array into one vector
-# y_pred_ravel = Y_pred.ravel()
-# lw = 2
-
-# fpr = dict()
-# tpr = dict()
-# roc_auc = dict()
-
-# for i in range(new_class):
-#     fpr[i], tpr[i], _ = roc_curve(Y_test[:, i], Y_pred[:, i])
-#     roc_auc[i] = auc(fpr[i], tpr[i])
+for i in range(new_class):
+    fpr[i], tpr[i], _ = roc_curve(Y_test[:, i], Y_pred[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
     
-# colors = cycle(['red', 'black'])
-# for i, color in zip(range(new_class), colors):
-#     plt.plot(fpr[i], tpr[i], color = color, lw = lw,
-#              label = 'ROC curve of class {0}'''.format(final_label[i]))
+colors = cycle(['red', 'black'])
+for i, color in zip(range(new_class), colors):
+    plt.plot(fpr[i], tpr[i], color = color, lw = lw,
+             label = 'ROC curve of class {0}'''.format(final_label[i]))
     
-# plt.plot([0, 1], [0, 1], 'k--', lw = lw)
-# plt.xlim([0, 1.0])
-# plt.ylim([0.0, 1.05])
-# plt.xlabel('False Positive Rate')
-# plt.ylabel('True Positive Rate')
-# plt.title('Receiver Operating Characteristic')
-# plt.legend(loc = "lower right")
-# plt.show()
+plt.plot([0, 1], [0, 1], 'k--', lw = lw)
+plt.xlim([0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc = "lower right")
+plt.show()
